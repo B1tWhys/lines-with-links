74bd1dd9efb0839fc4e15e367c16a5ca5e4945ab
skyler arnold <skyarnold1@me.com>
2022-12-31 18:45:10 -0700

    cleanup unused code

--- video-processing/video_processing/main.py
+++ video-processing/video_processing/main.py
@@ -18,41 +18,6 @@ log.setLevel(logging.DEBUG)
 
 app = typer.Typer()
 
-frame_source_queue = Queue()
-frames_queue = Queue(30)
-extraction_result_queue = Queue()
-
-
-def clear_queue(q):
-    while True:
-        try:
-            q.get_nowait()
-        except Empty:
-            break
-
-
-def video_streaming_worker():
-    log.info("Starting video streaming worker")
-    while True:
-        frame_source: FrameSource = frame_source_queue.get()
-        if frame_source is None:
-            break
-        for img in frame_source.stream_frames():
-            frame = Frame(frame_source.video_id, img, frame_source.current_sec_into_video)
-            log.info(f"Frame received: {frame.sec_into_video:0.2f}")
-            frames_queue.put(frame)
-    log.info("Video streaming worker exiting...")
-
-def position_persistance_worker():
-    log.info("Starting persistance worker")
-    while True:
-        result: FenExtractionResult = extraction_result_queue.get()
-        if result is None:
-            break
-        log.debug(f"extracted result: {result}")
-        save_position_sighting(result.vid_id, result.fen, result.sec_into_video)
-
-
 @app.command()
 def video(url: str,
           db_hostname: str = typer.Option("localhost", envvar="DB_HOSTNAME"),
@@ -86,36 +51,6 @@ def video(url: str,
                 log.info("Canceling worker task...")
                 task.stop()
                 worker_thread.join()
-    # try:
-    #     stream_thread.join()
-    #     log.info("Stream thread done")
-    #     frame_analyzer_thread.join()
-    #     log.info("Frame analyzer done")
-    #     extraction_result_queue.put(None)
-    #     position_persistance_thread.join()
-    #     log.info("Last positions persisted")
-    # except KeyboardInterrupt:
-    #     log.info("KeyboardInterrupt received, cleaning up workers...")
-    #
-    #     clear_queue(frame_source_queue)
-    #     frame_source_queue.put(None)
-    #     log.debug("frame_source_queue cleared")
-    #
-    #     clear_queue(frames_queue)
-    #     frames_queue.put(None)
-    #     log.debug("frames_queue cleared")
-    #
-    #     stream_thread.join()
-
-
-def initialize_threads():
-    stream_thread = Thread(target=video_streaming_worker)
-    frame_analyzer_thread = Thread(target=frame_analysis_worker, args=[frames_queue, extraction_result_queue])
-    position_persistence_thread = Thread(target=position_persistance_worker)
-    stream_thread.start()
-    frame_analyzer_thread.start()
-    position_persistence_thread.start()
-    return frame_analyzer_thread, stream_thread, position_persistence_thread
 
 
 if __name__ == "__main__":
